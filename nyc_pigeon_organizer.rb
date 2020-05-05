75c52a22942d5a2b19bbc80af923c2f5fcea89de
Lucas Leiberman <dr.lucasleibs@gmail.com>
2020-05-05 19:26:10 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,20 @@
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+
+  data.each do |color_gender_lives, attributes|
+    attributes.each do |stats, names|
+      names.each do |name|
+        pigeon_list[name] ||= {}
+        pigeon_list[name][color_gender_lives] ||= []
+        pigeon_list[name][color_gender_lives].push(stats.to_s)
+      end
+    end 
+  end
+
+  pigeon_list
 end
+ 
+ 
+ 
+
