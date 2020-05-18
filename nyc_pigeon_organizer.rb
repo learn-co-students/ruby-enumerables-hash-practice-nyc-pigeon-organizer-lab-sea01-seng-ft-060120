c8d1e28ecf09aa39aed57191ebc5d4111d7b5f15
Carly L <carlylynnla@gmail.com>
2020-05-18 00:52:45 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,21 @@
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_pigeon_list = {}
+  data.each do |color_gender_lives, inner_key|
+    inner_key.each do |key, array|
+       array.each do |name|
+        if new_pigeon_list.has_key?(name)
+          if new_pigeon_list[name].has_key?(color_gender_lives)
+            new_pigeon_list[name][color_gender_lives] << key.to_s 
+          else
+            new_pigeon_list[name][color_gender_lives] = [key.to_s]
+          end
+        else
+          new_pigeon_list[name] = {color_gender_lives => [key.to_s]}
+        end
+      end
+    end
+  end
+  new_pigeon_list
+
 end
